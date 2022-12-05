5ce7b19ed2a0665cb6461023f7feb8dc6059dc09
Thessa Kranendonk <thessakranendonk@gmail.com>
2022-12-05 13:03:27 -0500

    added a simple footer

---
+++ src/components/layout/project-layout/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer: React.FC = () => {
+  return (
+    <div className="w-screen flex justify-between bg-stone-900 text-white py-4 px-28">
+      <p className="font-serif">Runnymede Medical Center</p>
+      <p className="font-serif">
+        Walk-in Medical Clinic in Bloor Village, Toronto ON
+      </p>
+      <a href="tel:519-934-3434" className="font-serif">
+        416-800-9697
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
