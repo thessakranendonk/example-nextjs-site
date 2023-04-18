8fd51f189ac177a0f59ef5186cb49e7a36170cf0
Thessa Kranendonk <thessakranendonk@gmail.com>
2023-04-18 18:43:01 -0400

    added css for responsiveness

--- src/pages/policy.tsx
+++ src/pages/policy.tsx
@@ -3,31 +3,31 @@ import type { NextPage } from "next";
 const Policy: NextPage = () => {
   return (
     <>
-      <div className="w-mapXL mx-auto p-14">
-        <h1 className="text-4xl text-red-900 font-serif font-semibold pb-12">
+      <div className="lg:w-mapXL mx-auto p-14">
+        <h1 className="text-2xl lg:text-4xl text-brand-darkest font-semibold pb-12">
           The Clinic Policy
         </h1>
 
-        <h2 className="text-xl text-red-900 font-semibold font-serif py-2">
+        <h2 className="text-xl text-brand-darkest font-semibold py-2">
           Every Visit
         </h2>
-        <p className="text-lg pb-4 font-serif">
+        <p className="text-lg pb-4">
           Patient must bring and present a valid health card every visit. The
           government will not cover medical services without a valid heath card.
           A fee for the medical visit for none-OHIP patients must be paid prior
           to seeing the doctor. Official receipts are provided.
         </p>
-        <h2 className="text-xl text-red-900 font-semibold font-serif py-2">
+        <h2 className="text-xl text-brand-darkest font-semibold py-2">
           Update your own information
         </h2>
-        <p className="text-lg pb-4 font-serif">
+        <p className="text-lg pb-4">
           To serve you better patients must update their personal information
           including phone number /address and email address
         </p>
-        <h2 className="text-xl text-red-900 font-semibold font-serif py-2">
+        <h2 className="text-xl text-brand-darkest font-semibold py-2">
           Phone call or Email
         </h2>
-        <p className="text-lg pb-4 font-serif">
+        <p className="text-lg pb-4">
           We are unable to offer any medical advice or management over the phone
           or via email due to OHIP regulation and privacy conerns. Please
           contact us during the office hours and book an appointment with your
@@ -35,10 +35,10 @@ const Policy: NextPage = () => {
           immediate medical assistance, please go to the nearest hospital or
           call 911.
         </p>
-        <h2 className="text-xl text-red-900 font-semibold font-serif py-2">
+        <h2 className="text-xl text-brand-darkest font-semibold py-2">
           Uninsured Service
         </h2>
-        <p className="text-lg pb-4 font-serif">
+        <p className="text-lg pb-4">
           Not all medical services are covered by OHIP. Uninsured medical
           service and all third party requested forms are not covered by OHIP
           and will be charged to the patients. For example: sick notes,
